8169148e059d971e7b21a2a4c24d6a2920d199eb
pixelhijack <pothattila@gmail.com>
2016-08-05 22:10:42 +0200

    response transform multiplicator added, up to 500 for testing

--- dist/bundle.js
+++ dist/bundle.js
@@ -22113,15 +22113,27 @@
 	  getInitialState: function getInitialState() {
 	    return { data: [] };
 	  },
+	  responseTransform: function responseTransform(model, limit) {
+	    var sampleLength = model.length;
+	    for (var i = sampleLength, max = limit; i <= max; i++) {
+	      model.push({
+	        id: i + 1,
+	        name: model[Math.floor(Math.random() * sampleLength)].name,
+	        author: model[Math.floor(Math.random() * sampleLength)].author,
+	        genre: model[Math.floor(Math.random() * sampleLength)].genre,
+	        publish_date: model[Math.floor(Math.random() * sampleLength)].publish_date
+	      });
+	    }
+	    return model;
+	  },
 	  pullSomeAPI: function pullSomeAPI() {
-	    var data;
 	    var xhr = new XMLHttpRequest();
-	    var component = this;
 	    xhr.onload = function (e) {
-	      data = JSON.parse(this.responseText);
-	      console.log(data);
-	      component.setState({ data: data });
-	    };
+	      var response = JSON.parse(xhr.responseText);
+	      console.log(response);
+	      response = this.responseTransform(response, 500);
+	      this.setState({ data: response });
+	    }.bind(this);
 	    xhr.open('get', this.props.url, true);
 	    xhr.send();
 	  },
